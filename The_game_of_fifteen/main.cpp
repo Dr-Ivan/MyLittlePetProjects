e2e884cdf7c5b4a61d69ce1fb9b42bab77c8cd24
IvanDrv <ivander05@mail.ru>
2025-05-01 19:25:39 +0300

    Release version of game

--- The_game_of_fifteen/main.cpp
+++ The_game_of_fifteen/main.cpp
@@ -1,26 +1,10 @@
 #include <FL/Fl.h>
-#include <iostream>
-#include "GameView.hpp"
-#include "MatrixModel.hpp"
+#include "Controller.hpp"
 
-
-void printPair(std::pair<int, int> p){
-    std::cout << "Pressed button with coords: " << p.first << " /-/ " << p.second << std::endl;
-};
-
-void print(){
-    std::cout << " RESTART " << std::endl;
-};
 
 int main() {
-    MatrixModel m{};
-
-    GameView game {145, 220, "15", m};
-    game.setButtonGridCallback(printPair);
-    game.setRestartCallback(print);
-    game.update_window();
-    game.show();
-
+    Controller game{};
+    game.startGame();
     
     return Fl::run();
 }
